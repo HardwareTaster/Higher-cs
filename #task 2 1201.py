b2df3ef638ea0d5da8f94033b4e0682ccd6d68c6
HardwareTaster <a.artikov@albynschool.org>
2025-11-05 11:21:07 +0000

    Update #task 2 1201.py

--- #task 2 1201.py
+++ #task 2 1201.py
@@ -1,6 +1,12 @@
 #task 2 1201
-animal = input("enter name of the animal in lowercase please")
 
-newletter = chr(ord(animal[0])-32)
-newanimal = newletter+animal[1:]
-print(newanimal)
+def replace_letter(animal):
+  if ord(animal[0])>=97 and ord(animal[0])<=122:
+    newletter = chr(ord(animal[0])-32)
+    newanimal = newletter+animal[1:]
+    print(newanimal)
+  else:
+    print(animal)
+  
+animal = input("enter name of the animal in lowercase please")
+replace_letter(animal)
