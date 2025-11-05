dc1967c2cdaf2e778132712434a67273acfca38b
HardwareTaster <a.artikov@albynschool.org>
2025-11-05 11:43:19 +0000

    Update #task3 1201 predefined functions.py

--- #task3 1201 predefined functions.py
+++ #task3 1201 predefined functions.py
@@ -1,5 +1,10 @@
 #task3 1201 predefined functions
 password = input("enter password")
 while (ord(password[0:1])<65 or ord(password[0:1])>90) or (ord(password[-1])<35 or ord(password[-1])>37):
-    print("the first letter must be a capital letter and the password must end in #, $, or %")
+    if ord(password[0:1])<65 or ord(password[0:1])>90:
+        print("you need a capitol")
+    elif ord(password[-1])<35 or ord(password[-1])>37:
+         print("you need a #,$ or /%/ at the end")
+    else:
+         print("the first letter must be a capital letter and the password must end in #, $, or %")
     password = input("enter password")
