a981830a326fd52caa16ef4cb4533bf17c6b48bc
HardwareTaster <a.artikov@albynschool.org>
2025-11-06 09:34:44 +0000

    Add files via upload

---
+++ #task3 1101 file handling.py
@@ -0,0 +1,13 @@
+#task3 1101 file handling
+items = []
+names = []
+marks = []
+with open("file handlers/pupils.txt") as readfile:
+    line = readfile.readline().rstrip('\n')
+    while line:
+        items = line.split(",")
+        names.append(items[0])
+        marks.append(items[1])
+        line = readfile.readline().rstrip('\n')
+for i in range(len(names)):
+    print(names[i],"-",marks[i])
